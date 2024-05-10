4d7c3222c6f1e7997ab03d2ee1dcc2d59cf20d7b
UsmanovMikhail <umv2222@gmail.com>
2024-05-10 12:45:26 +0400

    Добавление файла

---
+++ Задача157.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    string str;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cout <<  string(i+1, '*') << endl;
+    }
+}
